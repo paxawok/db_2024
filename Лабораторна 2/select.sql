090b4d28ab9b575ede295a146e32741af81d2fde
paxawok <yurcherevach@gmail.com>
2024-04-12 12:54:59 +0300

    Lab1 & Lab2 done

--- Лабораторна 2/select.sql
+++ Лабораторна 2/select.sql
@@ -1,19 +1,25 @@
--- Active: 1707395589683@@127.0.0.1@5432@online_store
-SELECT "name", "id" FROM "Clients" WHERE "id" % 5 IN (1) AND "name" LIKE '%h';
-SELECT * FROM "Clients" WHERE "name" = 'Brandyn Picken';
-SELECT * FROM "Payments";
-UPDATE "Clients" SET "address" = '123 Main Street' WHERE "id" = 4;
-SELECT * FROM "Products" WHERE "category" = 'Casework';
-SELECT * FROM "Orders" WHERE "id" % 7 IN (0);
-INSERT INTO "Clients" ("id", "name", "address", "email", "phone")
-SELECT 
-    generate_series,
-    'name' || generate_series,
-    'address' || generate_series,
-    'email' || generate_series || '@example.com',
-    '1234567890'
-FROM 
-    generate_series(1001, 10000);
+-- Active: 1712562691271@@127.0.0.1@5432@online_store
+SELECT * FROM Clients WHERE name = 'Véronique';
+SELECT * FROM Payments;
+UPDATE Clients SET address = 'Suite 36' WHERE id = 12339532;
+SELECT * FROM Products WHERE category = 'Drama';
+SELECT * FROM Orders WHERE status = 'pending';
+SELECT category FROM Products WHERE price > 3900;
 
-SELECT * FROM "Orders" WHERE "status" = 'pending';
-SELECT "category" FROM "Products" WHERE "price" > 3900;
+SELECT AVG(amount) AS average_check FROM payments;
+
+SELECT Orders.id AS order_id, Clients.name AS client_name, Products.name AS product_name, Order_Details.quantity
+FROM Orders
+JOIN Clients ON Orders.client_id = Clients.id
+JOIN Order_Details ON Orders.id = Order_Details.order_id
+JOIN Products ON Order_Details.product_id = Products.id;
+
+SELECT Orders.id AS order_id, Clients.name AS client_name, 
+       SUM(Order_Details.quantity * Products.price) AS total_cost
+FROM Orders
+JOIN Clients ON Orders.client_id = Clients.id
+JOIN Order_Details ON Orders.id = Order_Details.order_id
+JOIN Products ON Order_Details.product_id = Products.id
+GROUP BY Orders.id, Clients.name;
+
+
